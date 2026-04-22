bf958ff521425268923692b9d3e22029eb713ef2
nishat <nishatlabiba98@gmail.com>
2026-04-21 20:03:50 -0400

    shapevillage

---
+++ src/main/java/rocks/zipcode/sproutroot/service/ShapeVillageService.java
@@ -0,0 +1,48 @@
+package rocks.zipcode.sproutroot.service;
+
+import org.springframework.stereotype.Service;
+import rocks.zipcode.sproutroot.dto.GameQuestion;
+import rocks.zipcode.sproutroot.model.*;
+import rocks.zipcode.sproutroot.repository.*;
+
+import java.util.*;
+
+@Service
+public class ShapeVillageService extends AbstractGameService {
+
+    public ShapeVillageService(
+            GameSessionRepository gameSessionRepository,
+            GameAnswerRepository gameAnswerRepository,
+            CurriculumContentRepository contentRepository,
+            MistakePatternRepository mistakePatternRepository,
+            ChildRepository childRepository) {
+        super(gameSessionRepository, gameAnswerRepository, contentRepository,
+              mistakePatternRepository, childRepository);
+    }
+
+    public GameSession startGame(UUID childId) {
+        return createSession(childId, GameType.SHAPE_VILLAGE);
+    }
+
+    @Override
+    public GameQuestion buildQuestion(UUID sessionId, int questionNumber) {
+        GameSession session = gameSessionRepository.findById(sessionId)
+                .orElseThrow(() -> new RuntimeException("Session not found"));
+
+        List<CurriculumContent> shapes = getContentByType(ContentType.SHAPE);
+        Collections.shuffle(shapes);
+        CurriculumContent picked = shapes.get(0);
+
+        GameQuestion q = new GameQuestion();
+        q.setSessionId(sessionId);
+        q.setContentId(picked.getId());
+        q.setQuestionText("Find the " + picked.getDisplayName() + " and place it in the scene!");
+        q.setCorrectAnswer(picked.getValue());
+        q.setChoices(buildChoices(picked.getValue(), shapes));
+        q.setPixabayKeyword(picked.getPixabayKeyword());
+        q.setQuestionNumber(questionNumber);
+        q.setTotalQuestions(QUESTIONS_PER_GAME);
+        q.setCurrentScore(session.getScore());
+        return q;
+    }
+}
